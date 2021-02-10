d24c0685f667f2a78809f1f0812f96eb2f8df39a
lolmos <lolmos@3oclock.co>
2021-02-10 22:08:33 +0100

    start

--- App.js
+++ App.js
@@ -2,6 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Storybook imports
+import { getStorybookUI, configure } from '@storybook/react-native';
+import { name as appName } from './app.json';
+import { AppRegistry } from 'react-native';
+
+configure(() => {
+  require('./src/stories.js'); // we will create this file in the next steps
+}, module);
+
+const StorybookUIRoot = getStorybookUI({});
+
 export default function App() {
   return (
     <View style={styles.container}>
---
+++ README.md
@@ -0,0 +1,8 @@
+# Alliance Athlete App
+
+Con:
+
+- React Native Web
+  - instalación en base a https://necolas.github.io/react-native-web/docs/?path=/docs/overview-getting-started--page
+- Storybook
+  - instalación en base a https://github.com/storybookjs/react-native/blob/master/app/react-native/docs/manual-setup.md
